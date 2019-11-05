b9271f313f3aafa223cb4c8e95dc1543404ad76e
franco948 <franco.palermo812@gmail.com>
2019-11-05 19:16:22 -0300

    Agregar imágenes panorámicas con StreetView

--- js/streetView.js
+++ js/streetView.js
@@ -5,6 +5,24 @@ streetViewModulo = (function () {
         /* Completar la función inicializar()  que crea un panorama
         en una posición y lo muestra en la página. */
 
+        // var fenway = {lat: 42.345573, lng: -71.098326};
+        var posicionInicial = posicionCentral
+
+        // var mapa = new google.maps.Map(document.getElementById('map'), {
+        //   center: fenway,
+        //   zoom: 14
+        // });
+
+        panorama = new google.maps.StreetViewPanorama(
+          document.getElementById('pano'), {
+            position: posicionInicial,
+            pov: {
+              heading: 0,
+              pitch: 0
+            }
+        });
+
+        mapa.setStreetView(panorama);
   }
 
     // Actualiza la ubicación del Panorama
@@ -12,6 +30,7 @@ streetViewModulo = (function () {
         /* Completar la función fijarStreetView que actualiza la posición
          de la variable panorama y cambia el mapa de modo tal que se vea
          el streetView de la posición actual. */
+         panorama.setPosition(mapa.getCenter())
   }
 
   return {
